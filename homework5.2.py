5275bf89a91f1c573a50f27e26931489a2b6476a
Aleksandr-ava <167477534+Aleksandr-ava@users.noreply.github.com>
2024-04-26 22:29:18 +0200

    Add files via upload

---
+++ homework5.2.py
@@ -0,0 +1,15 @@
+#my_list = ['orange', 'tangerine', 'plum', 'pomegranate', 'pineapple', 'kiwi', 'persimmon']
+#print(my_list)
+#print(my_list[0])
+#print(my_list[-1])
+#print(my_list[2:5])
+#my_list[2] = 'avocado'
+#print(my_list)
+
+my_dict = {'апельсин': 'orange', 'мандарин': 'tangerine', 'слива': 'plum'}
+print(my_dict)
+print(my_dict['мандарин'])
+my_dict['мандарин'] = 'жёлтый'
+print(my_dict)
+my_dict.update({'киви': 'kiwi'})
+print(my_dict)
